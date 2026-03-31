5eb2d21314eb87276d1569cbc3a68c2c51bd3e48
Roberto Keppler <rkeppler42@gmail.com>
2026-03-31 00:43:53 -0300

    fix(tp3): add right exercise 11

--- bloco-1-fundamentos-processamento-dados/python-intro/tp3/exercicio11.py
+++ bloco-1-fundamentos-processamento-dados/python-intro/tp3/exercicio11.py
@@ -1,23 +1,25 @@
-velocidade_do_vento = int(input("Qual a velocidade do vento?\n> "))
-temperatura = int(input("Qual a temperatura em graus Celsius?\n> "))
-umidade_do_ar = int(input("Qual a umidade do ar?\n> "))
-tipo_de_precipitacao = input("Qual o tipo de precipitação? ('Chuva', 'Neve', 'Granizo' ou 'Nenhum')").lower()
+import string
 
-if velocidade_do_vento > 80 or (tipo_de_precipitacao == "granizo" and velocidade_do_vento > 60):
-  print("ALERTA VERMELHO (Perigo Iminente)")
-elif (temperatura < -10
-      or (tipo_de_precipitacao == "neve" and velocidade_do_vento > 40)
-      or (tipo_de_precipitacao == "neve" and temperatura < 0)
-      or velocidade_do_vento > 50):
-  print("ALERTA LARANJA (Cuidado)")
-elif ((tipo_de_precipitacao == "chuva" and velocidade_do_vento > 40 and umidade_do_ar > 90)
-      or velocidade_do_vento > 20
-      or umidade_do_ar > 80): 
-  print("ALERTA AMARELO (Atenção)")
-elif ((tipo_de_precipitacao == "nenhum" and temperatura < 5 and velocidade_do_vento > 30)
-      or temperatura < 10
-      or umidade_do_ar > 75
-      or tipo_de_precipitacao == "chuva"):
-  print("ALERTA AZUL (Observação)")
+senha = input("Entre com sua senha:\n> ")
+contagem_numeros = 0
+tem_um_numero = False
+tem_caracter_especial = False
+
+
+
+for char in senha:
+  if char in "!@#":  # se tem pelo menos um dos caractéres especiais
+    tem_caracter_especial = True
+  elif char in string.digits:
+    contagem_numeros += 1  
+
+tem_um_numero = contagem_numeros == 1  # se tem 1 e apenas - segundo enunciado - 1 número
+
+caracteres_minimos = len(senha) >= 8  # se tem pelo menos 8 caractéres
+
+seguranca = [tem_caracter_especial, tem_um_numero, caracteres_minimos]
+
+if seguranca.count(True) >= 2:
+  print("Segura")
 else:
-  print("SEM ALERTA")
+  print("Fraca")
