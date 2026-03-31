6f4f0f690d86b4027c479236242bd3259a714ba4
Roberto Keppler <rkeppler42@gmail.com>
2026-03-31 00:18:48 -0300

    feat(tp2): add ex05 name badge exercise

---
+++ bloco-1-fundamentos-processamento-dados/python-intro/tp2/exercicio05.py
@@ -0,0 +1,7 @@
+nome_completo = input("Digite o nome completo:\n> ").upper()
+nome_cracha = input("Digite o nome que será exibido no crachá:\n> ").title()
+string_de_identificacao = f"{nome_cracha}---{nome_completo}"
+
+print(nome_completo)
+print(nome_cracha)
+print(string_de_identificacao)
