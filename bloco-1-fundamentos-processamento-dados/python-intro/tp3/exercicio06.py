f96a974b234bb358a9d89de5bb794f4f3a506ffe
Roberto Keppler <rkeppler42@gmail.com>
2026-03-31 00:35:28 -0300

    feat(tp3): add ex06 game skill unlock exercise

---
+++ bloco-1-fundamentos-processamento-dados/python-intro/tp3/exercicio06.py
@@ -0,0 +1,7 @@
+forca = int(input("Qual a força do personagem?\n> "))
+agilidade = int(input("Qual a agilidade do personagem?\n> "))
+
+if forca > 75 and agilidade > 80:
+  print("QUALIFICADO")
+else:
+  print("NAO QUALIFICADO")
