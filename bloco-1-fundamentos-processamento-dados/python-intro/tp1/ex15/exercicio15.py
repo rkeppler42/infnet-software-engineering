2bb1259eddd59c390ba42f3d388f0708a7f6bee0
Roberto Keppler <rkeppler42@gmail.com>
2026-03-31 00:09:05 -0300

    feat(tp1): add ex15 multi-step unit conversion exercise

---
+++ bloco-1-fundamentos-processamento-dados/python-intro/tp1/ex15/exercicio15.py
@@ -0,0 +1,6 @@
+voo_1 = 3.77 * 60 * 60
+voo_2 = 214 * 60
+voo_total = voo_1 + voo_2
+joules_total = 2.7 * voo_total
+joules_total_margem = joules_total * 1.15
+print(f"A quantidade total de joules é de {round(joules_total_margem, 2)} J")
