1d755b255af6efb68d23e98ee380e6b73dd34c57
Roberto Keppler <rkeppler42@gmail.com>
2026-03-31 00:49:05 -0300

    feat(at): add ex02 patient registration exercise

---
+++ bloco-1-fundamentos-processamento-dados/python-intro/at/exercicio02.py
@@ -0,0 +1,7 @@
+# Entradas
+nome = input("Qual nome completo do paciente?\n> ")
+idade = int(input("Qual a idade do paciente?\n> "))
+temperatura_corporal = float(input("Qual a temperatura do paciente?\n> "))
+
+# Saídas
+print(f"Nome: {nome} | Idade: {idade} | Temperatura: {temperatura_corporal}°C"
