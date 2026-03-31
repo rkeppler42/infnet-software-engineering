ded0745cb05ea97492daa5995625665526731d1d
Roberto Keppler <rkeppler42@gmail.com>
2026-03-31 00:22:13 -0300

    feat(tp2): add ex10 hashtag tracker exercise

---
+++ bloco-1-fundamentos-processamento-dados/python-intro/tp2/exercicio10.py
@@ -0,0 +1,6 @@
+texto = input()
+hashtag_capitalizada = texto.count("#PythonChallenge")
+hashtag_minuscula = texto.count("#pythonchallenge")
+
+print(f"#PythonChallenge: {hashtag_capitalizada}")
+print(f"#pythonchallenge: {hashtag_minuscula}")
